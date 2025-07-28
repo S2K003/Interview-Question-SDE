41bed01247f84a0b1ed9f2e51fb0ffae4334e236
Sharath St <sharath2k003@gmail.com>
2025-07-28 07:08:30 +0530

    Commit 08-7-28-07-2025_30.75-

--- problems/Question18/question.py
+++ problems/Question18/question.py
@@ -7,7 +7,7 @@ Given an integer array nums, return the number of reverse pairs in the array.
 A reverse pair is a pair (i, j) where:
 
 0 <= i < j < nums.length and nums[i] > 2 * nums[j].
- 
+
 Example 1:
 Input: nums = [1,3,2,3,1]
 Output: 2
