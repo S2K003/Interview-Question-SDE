b0cb1f33742eb71cc0daa28fe738a5d3708e96fd
Sharath St <s2k003@gmail.com>
2025-09-12 17:06:51 +1000

    Commit 06-17-12-09-2025_51.38-

---
+++ problems/Question9/answer.py
@@ -0,0 +1,37 @@
+Python Solution
+
+class MyHashSet(object):
+
+    def __init__(self):
+        self.set = [False] * 1000001
+
+    def add(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        self.set[key] = True
+        
+
+    def remove(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        self.set[key] = False
+        
+
+    def contains(self, key):
+        """
+        :type key: int
+        :rtype: bool
+        """
+        return self.set[key]
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
