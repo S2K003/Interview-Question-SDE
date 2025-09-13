8a35d6583605199dba6504fd68b9295f109a9ec3
Sharath St <s2k003@gmail.com>
2025-09-13 16:53:30 +1000

    Commit 53-16-13-09-2025_29.89-

---
+++ problems/Question14/answer.py
@@ -0,0 +1,21 @@
+Python Solution
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = [1] * len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix = prefix * nums[i]
+
+        postfix = 1
+        for i in range(len(nums)-1,-1,-1):
+            res[i] = res[i] * postfix
+            postfix = postfix * nums[i]
+
+        return res
