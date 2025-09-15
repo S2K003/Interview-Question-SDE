52eee88854149de81ffaee250dd2fee981ba5486
Sharath St <s2k003@gmail.com>
2025-09-15 23:43:19 +1000

    Commit 43-23-15-09-2025_19.62-

--- problems/Question17/answer.py
+++ problems/Question17/answer.py
@@ -12,3 +12,18 @@ class Solution(object):
                 profit += prices[i] - prices[i-1]
 
         return profit
+    
+Java Solution 
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for(int i=1;i<prices.length;i++)
+        {
+            if(prices[i]>prices[i-1])
+            {
+                profit = profit + prices[i] - prices[i-1];
+            }
+        }
+        return profit;
+    }
+}
